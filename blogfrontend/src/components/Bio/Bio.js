64d35835e058728b926e676fbc5ca8b54afe223d
Vinay Kushwaha <126447000+Vkushwahaa@users.noreply.github.com>
2025-03-08 15:55:00 +0000

    modified bio.js

--- blogfrontend/src/components/Bio/Bio.js
+++ blogfrontend/src/components/Bio/Bio.js
@@ -45,8 +45,8 @@ const Bio = ({ id,len }) => {
                 <img
                   src={
                     author.img
-                      ? `https://localhost-blog.onrender.com//${author.img}`
-                      : "https://localhost-blog.onrender.com//media/author_images/default.jpg"
+                      ? `https://localhost-blog.onrender.com/${author.img}`
+                      : "https://localhost-blog.onrender.com/media/author_images/default.jpg"
                   }
                   alt="Author Avatar"
                   className="rounded-circle bio-image"
